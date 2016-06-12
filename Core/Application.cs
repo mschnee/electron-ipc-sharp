74711c6448f52a5d02bb497e69656f061c6860b1
Rischerion <matthew.schnee@gmail.com>
2016-06-11 19:08:34 -0700

    Initial commit

---
+++ Core/Application.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Core
+{
+    public class Application
+    {
+        private volatile bool _running = true;
+
+        private int _killCount = 0;
+        public void Run()
+        {
+            Console.WriteLine("Starting Application Thread");
+            while (_running)
+            {
+                Thread.Sleep(1000);
+                _killCount++;
+
+                if (_killCount >= 10)
+                {
+                    _running = false;
+                }
+            }
+
+            Console.WriteLine("Ending Application Thread");
+        }
+    }
+}
---
+++ Core/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Program
+    {
+        static readonly Mutex singleInstanceMutex = new Mutex(true, "{0d716303-e857-41d1-acd5-f3556d720c8c}");
+
+        public static void Main(string[] args)
+        {
+            if (singleInstanceMutex.WaitOne(TimeSpan.Zero))
+            {
+                Initialize();
+                singleInstanceMutex.ReleaseMutex();
+            }
+            else
+            {
+                Console.WriteLine("Application already started.");
+            }
+        }
+
+        private static void Initialize()
+        {
+            var app = new Application();
+            var appThread = new Thread(app.Run);
+
+            appThread.Start();
+            appThread.Join();
+        }
+    }
+}
