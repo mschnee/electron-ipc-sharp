26dc5ab2f491c627e71078fd7dea90164c146e5f
Matthew Schnee <matthew.schnee@gmail.com>
2016-07-04 17:52:36 -0700

    runs on OSX

--- Core/Program.cs
+++ Core/Program.cs
@@ -8,19 +8,22 @@ namespace Core
 {
     public class Program
     {
-        static readonly Mutex singleInstanceMutex = new Mutex(true, "{ce0415be-eec2-42cb-bb17-3880031714d9}");
+        // this is window only :()
+        //static readonly Mutex singleInstanceMutex = new Mutex(true, "{ce0415be-eec2-42cb-bb17-3880031714d9}");
 
         public static void Main(string[] args)
         {
-            if (singleInstanceMutex.WaitOne(TimeSpan.Zero))
-            {
-                Initialize();
-                singleInstanceMutex.ReleaseMutex();
-            }
-            else
-            {
-                Console.WriteLine("Application already started.");
-            }
+            // if (singleInstanceMutex.WaitOne(TimeSpan.Zero))
+            // {
+                
+            //     singleInstanceMutex.ReleaseMutex();
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Application already started.");
+            // }
+            // write a lock file to determine if this is a single-application
+            Initialize();
         }
 
         private static void Initialize()
