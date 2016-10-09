ebeb8ec0a3d5850c7b5468d6d686f97aafef8907
Matthew Schnee <Matthew Schnee>
2016-10-08 18:57:16 -0700

    cleanup

--- source/Clients/ts/lib/Client.ts
+++
@@ -1,6 +0,0 @@
-export interface Client {
-    get<Response>(action: string): Promise<Response>;
-    put<Response>(action: string, data: any): Promise<Response>;
-    post<Response>(action: string, data: any): Promise<Response>;
-    delete<Response>(action: string): Promise<Response>;
-}
